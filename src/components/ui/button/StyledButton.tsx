c26c8e1cb21215bcc4c9bb0d0c4e452b1d8f498c
Innei <i@innei.in>
2024-04-27 14:43:55 +0800

    fix: forward button ref
    Signed-off-by: Innei <i@innei.in>

--- src/components/ui/button/StyledButton.tsx
+++ src/components/ui/button/StyledButton.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Fragment } from 'react'
+import { forwardRef, Fragment } from 'react'
 import clsx from 'clsx'
 import { tv } from 'tailwind-variants'
 import type { FC, PropsWithChildren } from 'react'
@@ -12,14 +12,14 @@ const variantStyles = tv({
     variant: {
       primary: clsx(
         'bg-accent text-zinc-100',
-        'active:contrast-125 hover:contrast-[1.10]',
+        'hover:contrast-[1.10] active:contrast-125',
         'font-semibold',
-        'disabled:bg-gray-400 disabled:opacity-30 disabled:dark:bg-gray-800 disabled:dark:text-zinc-50 disabled:cursor-not-allowed',
+        'disabled:cursor-not-allowed disabled:bg-gray-400 disabled:opacity-30 disabled:dark:bg-gray-800 disabled:dark:text-zinc-50',
         'dark:text-neutral-800',
       ),
       secondary: clsx(
         'group rounded-full bg-gradient-to-b from-zinc-50/50 to-white/90 px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:from-zinc-900/50 dark:to-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20',
-        'disabled:bg-gray-400 disabled:opacity-30 disabled:dark:bg-gray-800 disabled:dark:text-zinc-50 disabled:cursor-not-allowed',
+        'disabled:cursor-not-allowed disabled:bg-gray-400 disabled:opacity-30 disabled:dark:bg-gray-800 disabled:dark:text-zinc-50',
       ),
     },
   },
@@ -41,40 +41,49 @@ type ButtonProps = SharedProps &
       })
   )
 
-export const StyledButton: FC<ButtonProps> = ({
-  variant = 'primary',
-  className,
-  isLoading,
-  href,
+export const StyledButton = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      variant = 'primary',
+      className,
+      isLoading,
+      href,
 
-  ...props
-}) => {
-  const Wrapper = isLoading ? LoadingButtonWrapper : Fragment
+      ...props
+    },
+    ref,
+  ) => {
+    const Wrapper = isLoading ? LoadingButtonWrapper : Fragment
 
-  const Link = 'linkAs' in props ? props.linkAs || 'a' : 'a'
-  return (
-    <Wrapper>
-      {href ? (
-        <Link
-          href={href}
-          className={variantStyles({
-            variant,
-            className,
-          })}
-          {...(props as any)}
-        />
-      ) : (
-        <MotionButtonBase
-          className={variantStyles({
-            variant,
-            className,
-          })}
-          {...(props as any)}
-        />
-      )}
-    </Wrapper>
-  )
-}
+    const Link = 'linkAs' in props ? props.linkAs || 'a' : 'a'
+    return (
+      <Wrapper>
+        {href ? (
+          <Link
+            ref={ref as any}
+            href={href}
+            className={variantStyles({
+              variant,
+              className,
+            })}
+            {...(props as any)}
+          />
+        ) : (
+          <MotionButtonBase
+            ref={ref}
+            className={variantStyles({
+              variant,
+              className,
+            })}
+            {...(props as any)}
+          />
+        )}
+      </Wrapper>
+    )
+  },
+)
+
+StyledButton.displayName = 'StyledButton'
 
 const LoadingButtonWrapper: FC<PropsWithChildren> = ({ children }) => {
   return (
@@ -82,7 +91,7 @@ const LoadingButtonWrapper: FC<PropsWithChildren> = ({ children }) => {
       {children}
 
       <div className="absolute inset-0 z-[1] flex items-center justify-center">
-        <div className="loading loading-spinner h-5 w-5" />
+        <div className="loading loading-spinner size-5" />
       </div>
     </div>
   )
